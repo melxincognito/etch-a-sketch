8b34b33547d5ff6d31bc7ea73d4b2e3c42234eaa
Mel Incognito <melxincognito@gmail.com>
2021-08-27 18:38:04 -0700

    hover halfway working

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 for(x=0; x<= 255; x++) {
     var square = document.createElement('div');
     square.classList.add('block');
-    square.style.backgroundColor = 'white';
+    square.setAttribute('id', 'squares');
+    square.style.backgroundColor = 'lightyellow';
     square.style.width = '30px';
     square.style.height= '30px';
     document.getElementById('grid').appendChild(square);
 }
+
