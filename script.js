436f8d464a14c2fa76f8c924dc7858394b06c4b5
Mel Incognito <melxincognito@gmail.com>
2021-08-28 21:15:04 -0700

    hover working

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ for(x=0; x<= 255; x++) {
     var square = document.createElement('div');
     square.classList.add('block');
     square.setAttribute('id', 'squares');
-    square.style.backgroundColor = 'lightyellow';
     square.style.width = '30px';
     square.style.height= '30px';
+    
+    
     document.getElementById('grid').appendChild(square);
 }
-
