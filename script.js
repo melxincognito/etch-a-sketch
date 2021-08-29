bfcfcd10d5f648f2d911e320e463cbf10a12a7c0
Mel Incognito <melxincognito@gmail.com>
2021-08-29 15:43:18 -0700

    default grid size set on load

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+// constants and variables set
+
+const clearBoard = document.getElementById('clearBoard');
+const defaultGridSize = 16
 
 
-for(x=0; x<= 255; x++) {
-    var square = document.createElement('div');
-    square.classList.add('block');
-    square.setAttribute('id', 'squares');
-    square.style.width = '30px';
-    square.style.height= '30px';
-    square.addEventListener('mouseover', changeColor)
-    document.getElementById('grid').appendChild(square);
+
+
+// event listeners 
+
+clearBoard.addEventListener('click', () => { 
+    clear();
+});
+
+function setGrid(size) { 
+    for(x=0; x < size * size; x++) {
+        var square = document.createElement('div');
+        square.classList.add('block');
+        square.setAttribute('id', 'squares');
+        square.style.width = '30px';
+        square.style.height= '30px';
+        square.addEventListener('mouseover', changeColor)
+        document.getElementById('grid').appendChild(square);
     
+    }
 }
+
 
 function changeColor(e) {
     e.target.style.backgroundColor = 'lightyellow';
@@ -18,11 +33,10 @@ function changeColor(e) {
 
 function clear() {
     window.location.reload();
+    prompt('How many squares do you want in your next grid? (max 100)')
 
 }
 
-const clearBoard = document.getElementById('clearBoard');
-
-clearBoard.addEventListener('click', () => { 
-    clear();
-});
+window.onload = () => {
+    setGrid(defaultGridSize);
+}
