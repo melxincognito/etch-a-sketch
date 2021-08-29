b9af251b265b9843507345b1c2313ac5c1369636
Mel Incognito <melxincognito@gmail.com>
2021-08-28 22:00:21 -0700

    black trail on hover working

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ for(x=0; x<= 255; x++) {
     square.setAttribute('id', 'squares');
     square.style.width = '30px';
     square.style.height= '30px';
-    
-    
+    square.addEventListener('mouseover', changeColor)
     document.getElementById('grid').appendChild(square);
+    
 }
+
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+
